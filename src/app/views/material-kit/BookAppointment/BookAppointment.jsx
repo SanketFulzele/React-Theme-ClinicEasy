0fe3472ea8617f63fda77fcf48b63557299e011f
Sanket Fulzele <98740385+SanketFulzele@users.noreply.github.com>
2022-09-29 18:45:15 +0530

    React-Theme-ClinicEasy

---
+++ src/app/views/material-kit/BookAppointment/BookAppointment.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./bookAppointment.css";
+import Button from '@mui/material/Button';
+
+const BookAppointment = () => {
+    return (
+        <>
+
+
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </>
+    )
+}
+
+export default BookAppointment;
